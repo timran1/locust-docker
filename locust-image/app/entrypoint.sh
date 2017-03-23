a0f8770e788cf60a1058553ac70e27c4775d1697
Vincent De Smet <vincent.desmet@honestbee.com>
2017-03-23 14:40:29 +0800

    Make workers wait for master

--- locust-image/app/entrypoint.sh
+++ locust-image/app/entrypoint.sh
@@ -27,6 +27,11 @@ if [[ "$LOCUST_MODE" = "master" ]]; then
     LOCUST+=( --master)
 elif [[ "$LOCUST_MODE" = "worker" ]]; then
     LOCUST+=( --slave --master-host=$LOCUST_MASTER)
+    # wait for master
+    while ! wget -sqT5 $LOCUST_MASTER:$LOCUST_MASTER_WEB >/dev/null 2>&1; do
+        echo "Waiting for master"
+        sleep 5
+    done
 fi
 
 echo "${LOCUST[@]}"
